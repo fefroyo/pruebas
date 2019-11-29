1c364d511b6f5a6fb1842cbdd3c3ce1cf3ed4d9d
Efrain <5731936+fefroyo@users.noreply.github.com>
2019-11-29 13:59:18 +0100

    prueba

--- bot/prueba.php
+++ bot/prueba.php
@@ -1,5 +1,32 @@
 <?php 
 
-echo "Funciona !!";
+$botToken = "386844828:AAHZcFYFI-2E-ZlQPllTPIYBbd07CXhhX0c";
+$website = "https://api.telegram.org/bot".$botToken;
 
- ?>
+$update = file_get_contents('php://input');
+$update = json_decode($update, TRUE);
+
+$chatId = $update["message"]["chat"]["id"];
+$chatType = $update["message"]["chat"]["type"];
+
+$message = $update["message"]["text"];
+
+
+switch ($command) {
+	case '/ayuda':
+		$response = "Tranquilo, estoy contigo.";
+		sendMessage($chatId, $response);
+		break;
+	
+	default:
+		break;
+
+}
+
+function sendMessage($chatId,$response){
+
+	$url = $GLOBALS[website].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response);
+	file_get_contents($url);
+}
+
+?>
